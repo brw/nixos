47751062a8f680c1261fb70c1e778b6dba7be2ba
Bas van den Wollenberg <hi@bas.sh>
2026-08-02 00:55:54 +0200

    enable airplay discovery

--- config/audio.nix
+++ config/audio.nix
@@ -18,33 +18,33 @@
     };
 
     extraConfig.pipewire = {
-      # "10-airplay" = {
-      #   "context.modules" = [
-      #     {
-      #       name = "libpipewire-module-raop-discover";
-      #
-      #       args = {
-      #         roap.discover-local = true;
-      #         raop.latency.ms = 248.16;
-      #         stream.rules = [
-      #           {
-      #             matches = [
-      #               { raop.ip = "~.*"; }
-      #             ];
-      #             actions = {
-      #               create-stream = {
-      #                 stream.props = {
-      #                   media.class = "Audio/Sink";
-      #                   sess.latency.msec = "248.16";
-      #                 };
-      #               };
-      #             };
-      #           }
-      #         ];
-      #       };
-      #     }
-      #   ];
-      # };
+      "10-airplay" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-raop-discover";
+
+            args = {
+              roap.discover-local = true;
+              raop.latency.ms = 248.16;
+              stream.rules = [
+                {
+                  matches = [
+                    { raop.ip = "~.*"; }
+                  ];
+                  actions = {
+                    create-stream = {
+                      stream.props = {
+                        media.class = "Audio/Sink";
+                        sess.latency.msec = "248.16";
+                      };
+                    };
+                  };
+                }
+              ];
+            };
+          }
+        ];
+      };
     };
   };
 }
