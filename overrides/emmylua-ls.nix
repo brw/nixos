4b49d6c8cde07f00d3dc1c7b48385281864cabe7
Bas van den Wollenberg <hi@bas.sh>
2026-08-02 00:55:54 +0200

    emmylua-ls: 0.24.0 -> 0.25.1

--- overrides/emmylua-ls.nix
+++ overrides/emmylua-ls.nix
@@ -1,12 +1,25 @@
-{ pkgs }:
-pkgs.emmylua-ls.overrideAttrs (prevAttrs: {
-  version = "0.24.0";
-  src = prevAttrs.src.overrideAttrs {
-    hash = "sha256-oijk0SFTjpIak9AetBfctMXJ/72bRGjuaib9adx1yac=";
-  };
-  cargoDeps = prevAttrs.cargoDeps.overrideAttrs (prevAttrs: {
-    vendorStaging = prevAttrs.vendorStaging.overrideAttrs {
-      outputHash = "sha256-8uzkj7uCV3XzdagsrePwt2DE4MV12xlbwsaBONUymFQ=";
+{
+  pkgs,
+  lib ? pkgs.lib,
+}:
+let
+  version = "0.25.1";
+  srcHash = "sha256-wxVbjKHcid5rG4Y2xpq9AKu/T6tg8JGFfciChqvmUDo=";
+  cargoHash = "sha256-MAMHJDzU7WRfsl4IQ6eKt1mc1e90s2RQG8fwE8Ay3qc=";
+in
+if lib.versionOlder pkgs.emmylua-ls.version version then
+  pkgs.emmylua-ls.overrideAttrs (prevAttrs: {
+    inherit version;
+
+    src = prevAttrs.src.overrideAttrs {
+      hash = srcHash;
     };
-  });
-})
+
+    cargoDeps = prevAttrs.cargoDeps.overrideAttrs (prevAttrs: {
+      vendorStaging = prevAttrs.vendorStaging.overrideAttrs {
+        outputHash = cargoHash;
+      };
+    });
+  })
+else
+  pkgs.emmylua-ls
